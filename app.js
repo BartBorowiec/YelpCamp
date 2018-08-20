4c77c5099ea1eb172250dae3e12c3d8cfd58ca06
Bartek Borowiec <bartekborowiec91@gmail.com>
2018-08-20 07:07:45 +0000

    replace db url with env variable

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://bart:"+ process.env.DATABASE_PASSWORD +"@ds121182.mlab.com:21182/bartcamp");
+mongoose.connect(process.env.DATABASE_URL);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
